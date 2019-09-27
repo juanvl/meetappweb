e3d6340d3a3af7813f8f7be769cbc9be9398d7cc
Juan Victor Leite <jnvleite@gmail.com>
2019-09-27 10:32:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './config/ReactotronConfig';
+import GlobalStyles from 'styles/global';
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <Routes />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Main = () => <S.Container>Hello world!</S.Container>;
+
+export default Main;
